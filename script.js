5e0b4618fa2a7b9a184b9b40711f899a488a6675
Gabriel <gaboxroll.2@gmail.com>
2021-12-17 18:37:31 -0300

    Segundo Ejemplo

--- script.js
+++ script.js
@@ -6,6 +6,29 @@ function iniciar() {
     boton.addEventListener('click', clickBoton);
 }
 
-function clickBoton() {
-    alert(12312)
+async function cargarUrl(url) {
+   let response = fetch(url);
+   return (await response).json();
+}
+
+async function clickBoton() {
+    let json = await cargarUrl('https://api.covid19tracking.narrativa.com/api/2020-05-22/country/spain');
+    console.log(json)
+    
+    let estadisticas = json.dates["2020-05-22"].countries.Spain
+    console.log(estadisticas);
+
+
+
+    today_confirmed: 235290
+today_deaths: 28678
+today_hospitalised_patients_with_symptoms: 0
+today_intensive_care: 11454
+today_new_confirmed: 466
+today_new_deaths: 50
+today_new_hospitalised_patients_with_symptoms: 0
+today_new_intensive_care: 20
+today_new_open_cases: -381
+today_new_recovered: 797
+today_new_total_hospitalised_patients: 140
 }

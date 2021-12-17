2fc3ff13577dfed1143582184d5285832ed16188
Gabriel <gaboxroll.2@gmail.com>
2021-12-17 15:32:42 -0300

    Primer Ejemplo

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = iniciar;
+
+
+function iniciar() {
+    let boton = document.getElementById("btnCargar");
+    boton.addEventListener('click', clickBoton);
+}
+
+function clickBoton() {
+    alert(12312)
+}

387351b4aa81136c881ad0ee33e405ef60f77606
Gabriel <gaboxroll.2@gmail.com>
2021-12-17 19:00:38 -0300

    Tercer Ejemplo

--- script.js
+++ script.js
@@ -18,17 +18,27 @@ async function clickBoton() {
     let estadisticas = json.dates["2020-05-22"].countries.Spain
     console.log(estadisticas);
 
+    document.getElementById('today_confirmed').innerHTML = estadisticas.today_confirmed;
+    document.getElementById('today_deaths').innerHTML = estadisticas.today_deaths;
+    document.getElementById('today_hospitalised_patients_with_symptoms').innerHTML = estadisticas.today_hospitalised_patients_with_symptoms;
+    document.getElementById('today_intensive_care').innerHTML = estadisticas.today_intensive_care;
+    document.getElementById('today_new_confirmed').innerHTML = estadisticas.today_new_confirmed;
+    document.getElementById('today_new_deaths').innerHTML = estadisticas.today_new_deaths;
+    document.getElementById('today_new_hospitalised_patients_with_symptoms').innerHTML = estadisticas.today_new_hospitalised_patients_with_symptoms;
+    document.getElementById('today_new_intensive_care').innerHTML = estadisticas.today_new_intensive_care;
+    document.getElementById('today_new_open_cases').innerHTML = estadisticas.today_new_open_cases;
+    document.getElementById('today_new_recovered').innerHTML = estadisticas.today_new_recovered;
+    document.getElementById('today_new_total_hospitalised_patients').innerHTML = estadisticas.today_new_total_hospitalised_patients;
 
-
-    today_confirmed: 235290
-today_deaths: 28678
-today_hospitalised_patients_with_symptoms: 0
-today_intensive_care: 11454
-today_new_confirmed: 466
-today_new_deaths: 50
-today_new_hospitalised_patients_with_symptoms: 0
-today_new_intensive_care: 20
-today_new_open_cases: -381
-today_new_recovered: 797
-today_new_total_hospitalised_patients: 140
+// today_confirmed
+// today_deaths
+// today_hospitalised_patients_with_symptoms
+// today_intensive_care
+// today_new_confirmed
+// today_new_deaths
+// today_new_hospitalised_patients_with_symptoms
+// today_new_intensive_care
+// today_new_open_cases
+// today_new_recovered
+// today_new_total_hospitalised_patients
 }
